ca972c12a60c77038765abcb2704e8e36af3f415
Jobson Tenório <jobsontenorio@gmail.com>
2022-08-03 10:09:02 -0300

    Exemplo 1 - pug

---
+++ jQuery e Select2/Express e pug/src/app.js
@@ -0,0 +1,21 @@
+// Importação CommonJS modules
+const express = require('express');
+const path = require('path');
+
+// Criar aplicação WEB express
+const app = express();
+
+// view engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+// Arquivos estáticos
+app.use(express.static(path.join(process.cwd(), 'public')));
+
+//Rotas
+app.get('/', (req, res) => {
+  res.status(200).render('index');
+});
+
+// Rodar a aplicação express na porta 3000
+app.listen(3000);
